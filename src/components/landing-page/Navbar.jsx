f888b9e651acdc7faf9a2ae65d5fada4d15988f3
Ivan Gomez <158605517+IvanDaGomez@users.noreply.github.com>
2025-12-26 23:37:49 -0500

    Remove unused social media icons from Navbar (#30)
    Co-authored-by: Fatima <64161795+hrh-zara@users.noreply.github.com>

--- src/components/landing-page/Navbar.jsx
+++ src/components/landing-page/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+
 import { Linkedin, Instagram, Menu, X } from 'lucide-react';
+
 import gsap from 'gsap';
 import { heroAssest, mainsong } from './utils';
 
