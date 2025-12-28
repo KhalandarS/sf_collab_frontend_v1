56d34823743acc9a474596381bda7d06984543d8
Fatima <64161795+hrh-zara@users.noreply.github.com>
2025-12-28 18:36:26 +0100

    Update contact email address in Contact.jsx (#68)

--- src/components/landing-page/pages/Contact.jsx
+++ src/components/landing-page/pages/Contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
                 </p> */}
                 <p className="flex items-center gap-4">
                   <Mail className="w-5 h-5 text-gray-400" />
-                  <a href="hello@sfcollab.com" className="hover:text-white transition">hello@sfcollab.com</a>
+                  <a href="SFCOLLAB33@GMAIL.COM" className="hover:text-white transition">SFCOLLAB33@GMAIL.COM</a>
                 </p>
                 {/* <p className="flex items-center gap-4">
                   <Phone className="w-5 h-5 text-gray-400" />
